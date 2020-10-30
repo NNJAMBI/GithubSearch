59f480840075ffa561462f5220f647dc2323e42e
Nancy Njambi Karanja <nnjambi@safaricom.co.ke>
2020-10-30 16:15:27 +0300

    add changes made to the time ago pipe

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -7,19 +7,10 @@ import { ProfileService } from '../profile-service/profile.service';
   styleUrls: ['./landing-page.component.css']
 })
 export class LandingPageComponent implements OnInit {
-  // user;
-  // repos;
+
   constructor(private service: ProfileService) { }
 
   ngOnInit(): void {
-    // this.service.getUserInfo().subscribe(res => {
-    //   console.log(res)
-    //   this.user = res
-    // })
-    // this.service.getReposInfo().subscribe(res => {
-    //   console.log(res);
-    //   this.repos = res;
-    // })
 
   }
 
